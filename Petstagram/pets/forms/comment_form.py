608d2188a5ba874d052eb704b1970ae6576d0414
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-05-16 15:12:53 +0300

    updates - edit delete

--- Petstagram/pets/forms/comment_form.py
+++ Petstagram/pets/forms/comment_form.py
@@ -3,7 +3,16 @@ from django import forms
 from pets.models import Comment
 
 
-class CommentForms(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = '__all__'
+# class CommentForms(forms.ModelForm):
+#     class Meta:
+#         model = Comment
+#         fields = '__all__'
+
+class CommentForm(forms.Form):
+    text = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control rounded-2'
+            }
+        )
+    )
--- Petstagram/pets/views.py
+++ Petstagram/pets/views.py
@@ -1,10 +1,10 @@
-
-
 # Create your views here.
+from django import forms
 from django.shortcuts import render, redirect
 
-from pets.forms.comment_form import CommentForms
-from pets.models import Pet, Like
+from pets.forms.comment_form import CommentForm, CommentForm
+from pets.forms.pet_form import PetForm
+from pets.models import Pet, Like, Comment
 
 
 def list_pets(request):
@@ -15,15 +15,26 @@ def list_pets(request):
     return render(request, 'pet_list.html', context)
 
 
-def show_pet_details(request, pk):
+def details_or_comment_pet(request, pk):
     pet = Pet.objects.get(pk=pk)
-    pet.likes_count = pet.like_set.count()
-    context = {
-        'pet': pet,
-        'form': CommentForms(),
-    }
-
-    return render(request, 'pet_detail.html', context)
+    # pet.likes_count = pet.like_set.count()
+    if request.method == 'GET':
+        context = {
+            'pet': pet,
+            'form': CommentForm(),
+        }
+        return render(request, 'pet_detail.html', context)
+    else:
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(text=form.cleaned_data['text'])
+            comment.pet = pet
+            comment.save()
+            return redirect('pet details or comment', pk)
+        context = {
+            'pet': pet,
+            'form': form,
+        }
 
 
 def like_pet(request, pk):
@@ -32,3 +43,43 @@ def like_pet(request, pk):
     like.pet = pet
     like.save()
     return redirect('pet details', pk)
+
+
+def edit_pet(request, pk):
+    pet = Pet.objects.get(pk=pk)
+    if request.method == "GET":
+        form = PetForm(instance=pet)
+
+        context = {
+            'form': form,
+            'pet': pet,
+        }
+
+        return render(request, 'pet_edit.html', context)
+    else:
+        form = PetForm(
+            request.POST,
+            instance=pet
+        )
+        if form.is_valid():
+            form.save()
+            return redirect('pet details or comment', pet.pk)
+        
+        context = {
+            'form': form,
+            'pet': pet,
+        }
+        return render(request, 'pet_edit.html', context)
+
+
+def delete_pet(request, pk):
+
+    pet = Pet.objects.get(pk=pk)
+    if request.method == 'GET':
+        context = {
+            'pet': pet,
+        }
+        return render(request, 'pet_delete.html', context)
+    else:
+        pet.delete()
+        return redirect('list pets')
